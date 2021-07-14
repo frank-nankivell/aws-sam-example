d4fe23e0c911adffcb4bdab75afbeaea5cb4edea
frank <frank@havuta.com>
2021-07-14 11:29:25 +0100

    Adding initial PR

---
+++ functions/example.js
@@ -0,0 +1,19 @@
+exports.handler = async (event) => {
+  const { httpMethod } = event;
+
+  if (httpMethod !== "GET") {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({
+        status: 405,
+        title: "Method not allowed",
+        message: `Endpoint only accept GET method, you tried: ${httpMethod}`,
+      }),
+    };
+  }
+  console.log(`event: ${event}`);
+  return {
+    statusCode: 200,
+    body: JSON.stringify("Hello World"),
+  };
+};
